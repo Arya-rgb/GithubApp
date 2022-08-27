feccff27ca747930dda50e6f330d0054bdc6c432
Dicky Arya Pratama <dickyarya.personal@gmail.com>
2022-08-27 12:32:29 +0700

    preparing remote data source
    remote data source for remote data using retrofit

---
+++ app/src/main/java/com/kompas/githubapp/data/remote/response/ResultResponse.kt
@@ -0,0 +1,14 @@
+package com.kompas.githubapp.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class UserResult(
+    @field:SerializedName("login")
+    val login: String,
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("avatar_url")
+    val avatar_url: String,
+)
---
+++ app/src/main/java/com/kompas/githubapp/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,14 @@
+package com.kompas.githubapp.data.remote.retrofit
+
+import com.kompas.githubapp.data.remote.response.UserResult
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface ApiService {
+
+    @GET("users")
+    suspend fun getUserList(
+        @Header("Authorization") token: String,
+    ): UserResult
+
+}
